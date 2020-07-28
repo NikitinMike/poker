b81120c6e69a194a56edbaad99f2596210d533ce
Mikhail Nikitin <36443086+NikitinMike@users.noreply.github.com>
2020-07-28 21:44:01 +0300

    rename variables to describe

--- src/main/java/com/example/democards/PokerApplication.java
+++ src/main/java/com/example/democards/PokerApplication.java
@@ -35,7 +35,7 @@ public class PokerApplication {
         StringBuilder cards = new StringBuilder();
         for (int i = 0; i < 5; i++)
             cards.append(getSuit(img, fieldLeft + 20 + cardWidth * i, fieldUp + 43))
-                    .append(getValue(img, i, fieldLeft + cardWidth * i, fieldUp, 33, 26));
+                .append(getValue(img, i, fieldLeft + cardWidth * i, fieldUp, 33, 26));
         System.out.println(file.getName() + " " + cards.toString());
     }
 
@@ -49,8 +49,8 @@ public class PokerApplication {
     static String getValue(BufferedImage img, int pos, int x, int y, int w, int h) {
         Color c = new Color(img.getRGB(x + w, y + 1));
         if (!c.equals(Color.WHITE) && !c.equals(SELECTEDRGB)) return "";
-        int ss = 0;
-        int sf = 0;
+        int sharp = 0;
+        int fuzzy = 0;
         int j = 0;
         int[] res = new int[h + 1];
         int[] data = img.getRGB(x, y, w, h, null, 0, w);
@@ -58,11 +58,11 @@ public class PokerApplication {
             if ((i % w) == 0) j++;
             if (data[i] == white || data[i] == selected) continue;
             if (checkArea(img, white, x + i % w, y + j)
-                || checkArea(img, selected, x + i % w, y + j)) sf++;
-            else ss++;
+                || checkArea(img, selected, x + i % w, y + j)) fuzzy++;
+            else sharp++;
             res[j]++;
         }
-        return getRank(pos, sf, ss, res[11] + res[12] + res[13] + res[14]);
+        return getRank(pos, fuzzy, sharp, res[11] + res[12] + res[13] + res[14]);
     }
 
     private static boolean checkArea(BufferedImage img, int color, int x, int y) {
@@ -87,14 +87,14 @@ public class PokerApplication {
         return " ";
     }
 
-    static String getRank(int pos, int sf, int ss, int sa) {
-        int s = ss + sf;
-        if (100 <= sf && sf <= 109) return "4";
+    static String getRank(int pos, int fuzzy, int sharp, int a235) {
+        int s = sharp + fuzzy;
+        if (100 <= fuzzy && fuzzy <= 109) return "4";
         if (269 <= s && s <= 279)
-            return (pos == 0 && sf == 137) ? "9" : "6";
-        if (pos == 0 && sf == 136) return "6";
+            return (pos == 0 && fuzzy == 137) ? "9" : "6";
+        if (pos == 0 && fuzzy == 136) return "6";
         if (167 <= s && s <= 177) return "7";
-        if (144 <= sf && sf <= 149) return "8";
+        if (144 <= fuzzy && fuzzy <= 149) return "8";
         if (280 <= s && s <= 289) return "9";
         if (380 <= s && s <= 406) return "10";
         if (153 <= s && s <= 161) return "J";
@@ -103,9 +103,9 @@ public class PokerApplication {
         if (pos == 1 && s == 244) return "K";
         if (pos == 2 && s == 251) return "K";
         if (248 <= s && s <= 255) return "A";
-        if (135 <= sf && sf <= 137) return "K";
-        if (120 <= sf && sf <= 130)
-            return sa < 30 ? "2" : sa < 50 ? "3" : sa < 70 ? "5" : " ";
+        if (135 <= fuzzy && fuzzy <= 137) return "K";
+        if (120 <= fuzzy && fuzzy <= 130)
+            return a235 < 30 ? "2" : a235 < 50 ? "3" : a235 < 70 ? "5" : " ";
         return " ";
     }
 
